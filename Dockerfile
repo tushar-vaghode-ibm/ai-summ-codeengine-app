23ca5a0f46b7822d5eed0e7ff2a64fc622613e8c
Tushar Vaghode <Tushar.Vaghode@ibm.com>
2024-07-05 19:02:29 -0700

    Beta 2 - UI update. Performance improvement.

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,18 @@ FROM docker.io/library/python:3.12.4
 MAINTAINER Tushar Vaghode <Tushar.Vaghode@ibm.com>
 EXPOSE 8501
 
+# Install required modules
 RUN \
     apt-get update && \
     pip install --upgrade pip && \
-    pip install ibm-watsonx-ai && \
-    pip install streamlit && \
+    pip install ibm-watsonx-ai==1.0.10 && \
+    pip install streamlit==1.36.0 && \
     mkdir /codeengine
 
-COPY app.py /codeengine/
-COPY .streamlit /codeengine/
+# Copy code
+COPY app.py /codeengine/app.py
+COPY .streamlit /codeengine/.streamlit
 
+# Run
 WORKDIR /codeengine
 CMD streamlit run app.py
--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from ibm_watsonx_ai.foundation_models import Model
 import json
 import streamlit as st
 
+st.header("AI summarization using serverless architecture in IBM Cloud")
+prompt = st.chat_input("Ask something")
+
 # import vairables
 import os
 WATSONX_URL = os.environ.get('WATSONX_URL', 'https://us-south.ml.cloud.ibm.com')
@@ -10,44 +13,31 @@ WATSONX_APIKEY = os.environ.get('WATSONX_APIKEY')
 WATSONX_LLM_ID = os.environ.get('DEFAULT_WATSONX_MODEL_ID', 'GRANITE_13B_CHAT_V2')
 WATSONX_PROJECT_ID = os.environ.get('WATSONX_PROJECT_ID')
 
-# Set page configuration
-st.set_page_config(
-  page_title="AI Summarization using serverless architecture in IBM Cloud",
-  page_icon="🌐",
-  layout="wide",
-  initial_sidebar_state="expanded",
-)
-
-# Add a title and a subtitle
-st.title("AI Summarization using serverless architecture in IBM Cloud")
-
-# st.header("Input Section")
-with st.form(key='myform'):
-  input_value = st.text_area("Ask a question or enter your input text for summarization:", "What are key capabilities of IBM watsonx.ai?", height=50)
-  submit_button = st.form_submit_button(label="Submit")
-
 # setup watsonx.ai connection
-my_credentials = {
-    "url"    : WATSONX_URL,
+def model_init():
+  my_credentials = {
+    "url" : WATSONX_URL,
     "apikey" : WATSONX_APIKEY
-}
-llm_model   = getattr(ModelTypes, WATSONX_LLM_ID)
-project_id  = WATSONX_PROJECT_ID
-space_id    = None
-verify      = False
-parameters  = {
-  "decoding_method": "greedy",
-  "max_new_tokens": 2000,
-  "min_new_tokens": 100,
-  "repetition_penalty": 1
- }
-model = Model(llm_model, my_credentials, parameters, project_id, space_id, verify)
+  }
+  llm_model   = getattr(ModelTypes, WATSONX_LLM_ID)
+  project_id  = WATSONX_PROJECT_ID
+  space_id    = None
+  verify      = False
+  parameters  = {
+    "decoding_method": "greedy",
+    "max_new_tokens": 2000,
+    "min_new_tokens": 100,
+    "repetition_penalty": 1
+  }
+  model = Model(llm_model, my_credentials, parameters, project_id, space_id, verify)
+  return model
 
+# output section
+output = ""
+if prompt:
+  model = model_init()
+  output = model.generate_text(prompt)
 
-# Define a function to process the input
-def process_input(input_value):
-  return model.generate_text(input_value)
-
-# Display the output
-# st.header("Output Section")
-st.text_area("AI generated output:", process_input(input_value), height=400, disabled=True)
+st.html("<h4 style='color: gray'>AI generated output:</h1>")
+with st.container(border=True, height=500):
+  st.write(output)
